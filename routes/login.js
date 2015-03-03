96241c00b7cc7e14189e547466ec520aa34e08f9
australiansteve <steve@australiansteve.com>
2015-03-02 23:41:52 -0400

    First attempt at communicating with IG

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
 var config = require('./../config.js');
 
@@ -9,8 +10,24 @@ router.get('/', function(req, res) {
 
   //https://api.instagram.com/oauth/authorize/?client_id=3b0c2cadab27402a90302919d12eeccd&redirect_uri=http://seethisspot.herokuapp.com/login&response_type=code&scope=likes
 
-  res.setHeader('Content-Type', 'text/html');
-  res.end("<html><body>Hi</body></html>");
+  var options = {
+    host: 'https://api.instagram.com',
+    port: 80,
+    path: '/oauth/authorize/?client_id='+ config.instagram.client_id +'&redirect_uri='+ config.instagram.redirect_url +'&response_type=code&scope=likes',
+    method: 'POST'
+  };
+
+  http.request(options, function(instaResponse) {
+    console.log('STATUS: ' + instaResponse.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(instaResponse.headers));
+    instaResponse.setEncoding('utf8');
+    instaResponse.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+  }).pipe(res);
+
+  //res.setHeader('Content-Type', 'text/html');
+  //res.end("<html><body>Hi</body></html>");
 });
 
 module.exports = router;
